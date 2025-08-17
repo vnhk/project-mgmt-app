51c97832569a78f4bd163310eca470cf96aa4369
berluk12@gmail.com <berluk12@gmail.com>
2025-08-17 14:27:17 +0200

    New Navigation menu styling

--- src/main/java/com/bervan/projectmgmtapp/views/ProjectsPageLayout.java
+++ src/main/java/com/bervan/projectmgmtapp/views/ProjectsPageLayout.java
@@ -1,15 +1,16 @@
 package com.bervan.projectmgmtapp.views;
 
 import com.bervan.common.MenuNavigationComponent;
+import com.vaadin.flow.component.icon.VaadinIcon;
 
 public class ProjectsPageLayout extends MenuNavigationComponent {
     public ProjectsPageLayout(String route, String... notVisibleButtons) {
         super(route, notVisibleButtons);
 
-        addButtonIfVisible(menuButtonsRow, AbstractProjectListView.ROUTE_NAME, "Projects");
-        addButtonIfVisible(menuButtonsRow, AbstractAllTasksListView.ROUTE_NAME, "All Tasks");
-        addButtonIfVisible(menuButtonsRow, AbstractProjectDetailsView.ROUTE_NAME, "Project");
-        addButtonIfVisible(menuButtonsRow, AbstractTaskDetailsView.ROUTE_NAME, "Task");
+        addButtonIfVisible(menuButtonsRow, AbstractProjectListView.ROUTE_NAME, "Projects", VaadinIcon.HOME.create());
+        addButtonIfVisible(menuButtonsRow, AbstractAllTasksListView.ROUTE_NAME, "All Tasks", VaadinIcon.HOME.create());
+        addButtonIfVisible(menuButtonsRow, AbstractProjectDetailsView.ROUTE_NAME, "Project", VaadinIcon.HOME.create());
+        addButtonIfVisible(menuButtonsRow, AbstractTaskDetailsView.ROUTE_NAME, "Task", VaadinIcon.HOME.create());
 
         add(menuButtonsRow);
     }
