916670b9d751ccdd66d92d45df0f42c4e7b8a68f
berluk12@gmail.com <berluk12@gmail.com>
2025-08-17 14:35:06 +0200

    New Navigation menu icons

--- src/main/java/com/bervan/projectmgmtapp/views/ProjectsPageLayout.java
+++ src/main/java/com/bervan/projectmgmtapp/views/ProjectsPageLayout.java
@@ -7,10 +7,10 @@ public class ProjectsPageLayout extends MenuNavigationComponent {
     public ProjectsPageLayout(String route, String... notVisibleButtons) {
         super(route, notVisibleButtons);
 
-        addButtonIfVisible(menuButtonsRow, AbstractProjectListView.ROUTE_NAME, "Projects", VaadinIcon.HOME.create());
-        addButtonIfVisible(menuButtonsRow, AbstractAllTasksListView.ROUTE_NAME, "All Tasks", VaadinIcon.HOME.create());
-        addButtonIfVisible(menuButtonsRow, AbstractProjectDetailsView.ROUTE_NAME, "Project", VaadinIcon.HOME.create());
-        addButtonIfVisible(menuButtonsRow, AbstractTaskDetailsView.ROUTE_NAME, "Task", VaadinIcon.HOME.create());
+        addButtonIfVisible(menuButtonsRow, AbstractProjectListView.ROUTE_NAME, "Projects", VaadinIcon.FOLDER.create());
+        addButtonIfVisible(menuButtonsRow, AbstractAllTasksListView.ROUTE_NAME, "All Tasks", VaadinIcon.TASKS.create());
+        addButtonIfVisible(menuButtonsRow, AbstractProjectDetailsView.ROUTE_NAME, "Project", VaadinIcon.INFO_CIRCLE.create());
+        addButtonIfVisible(menuButtonsRow, AbstractTaskDetailsView.ROUTE_NAME, "Task", VaadinIcon.CLIPBOARD_TEXT.create());
 
         add(menuButtonsRow);
     }
